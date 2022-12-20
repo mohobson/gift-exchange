bd10c3ab90f4ffee41c26fcf4ab1aebff4802c5a
Mo Hobson <mohobson8@gmail.com>
2022-12-19 23:42:19 -0500

    added basic form

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+messages = [{'title': 'Message One',
+             'content': 'Message One Content'},
+            {'title': 'Message Two',
+             'content': 'Message Two Content'}
+            ]
 
 @app.route('/')
 def hello_world():
-    return 'Gift Exchange'
+    return render_template('index.html', messages=messages)
+
+
+#https://www.digitalocean.com/community/tutorials/how-to-use-web-forms-in-a-flask-application
