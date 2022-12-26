264eb5308b26d2a156441f2e2c81a063899a74a3
Mo Hobson <mohobson8@gmail.com>
2022-12-26 15:42:43 -0500

    added static

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-messages = [{'title': 'Message One',
-             'content': 'Message One Content'},
-            {'title': 'Message Two',
-             'content': 'Message Two Content'}
+messages = [{'name': 'name one',
+             'email': 'email one'},
+            {'name': 'name two',
+             'email': 'email two'}
             ]
 
 @app.route('/')
