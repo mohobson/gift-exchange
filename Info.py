a05bfd528a0ddf6bf17b3980d2253081af1939f5
Mo Hobson <mohobson8@gmail.com>
2022-12-29 01:12:50 -0500

    revising forms

---
+++ Info.py
@@ -0,0 +1,4 @@
+class Info:
+    def __init__(self, title, email=None):
+        self.title = title
+        self.email = email
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, request
+
+import datetime
 
 messages = [{'name': 'name one',
              'email': 'email one'},
@@ -7,9 +8,39 @@ messages = [{'name': 'name one',
              'email': 'email two'}
             ]
 
-@app.route('/')
-def index():
-    return render_template('index.html', messages=messages)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("settings")
+
+    @app.route('/')
+    def index():
+        # today = datetime.datetime()
+        # day_name = today.strftime("%A")
+        day_name='dayname'
+        print(day_name)
+        return render_template('index.html', day=day_name, messages=messages)
+
+    @app.route('/info')
+    def info_page():
+        return render_template("info.html")
+
+    @app.route('/info')
+    def info_add_page():
+        if request.method == "GET":
+            return render_template(
+                "info.html"
+            )
+            
+        else:
+            form_title = request.form["title"]
+            person_name = request.form["name"]
+            # email = Email(form_title, name=person_name if person_name else None)
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    port = app.config.get("PORT", 5000)
+    app.run(host="0.0.0.0", port=port)
 
 
 #https://www.digitalocean.com/community/tutorials/how-to-use-web-forms-in-a-flask-application
