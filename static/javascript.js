18b31470f088239ceefe09268faa8f87b29ae31c
Mo Hobson <mohobson8@gmail.com>
2023-02-08 18:12:24 -0500

    added snowflakes

---
+++ static/javascript.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    for (var i = 0; i < 50; i++) {
+      var snowflake = document.createElement("div");
+      snowflake.classList.add("snowflake");
+      snowflake.style.left = Math.random() * 100 + "%";
+      snowflake.style.animationDelay = Math.random() * 5 + "s";
+      snowflake.style.animationDuration = Math.random() * 10 + 5 + "s";
+      snowflake.style.animationTimingFunction = "ease-in-out";
+      document.getElementById("snowflakesContainer").appendChild(snowflake);
+    }
+  };
